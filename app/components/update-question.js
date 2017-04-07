a1bfafea6cf8b748732d7767e54d85b7cbf3bd8e
Morris <mburumorris@gmail.com>
2017-04-07 15:00:38 +0300

    add a question route.

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        user: this.get('user'),
+        content: this.get('content')
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
 });
